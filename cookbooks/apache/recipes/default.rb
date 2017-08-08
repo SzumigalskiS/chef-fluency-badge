733d735f6bc827ffae15a49f15f2b7c085d3942d
Sebastian Szumigalski <sebastian@szumigalski.com>
2017-08-08 08:41:53 +0800

    cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,11 +6,12 @@
 
 
 package 'apache2' do
-    package_name 'httpd'
-    action :install
+  package_name 'httpd'
+  action :install
 end
 
 service 'apache2' do
-    service_name 'httpd'
-    action [:start, :enable]
+  service_name 'httpd'
+  action [:start, :enable]
 end
+
